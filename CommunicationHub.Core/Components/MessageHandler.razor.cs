c1ca4a37bf6b5d398be5cd3b3c12408671a0ee05
JDK11 <jdekock1184@gmail.com>
2023-06-15 10:59:55 +0200

    Added application files

---
+++ CommunicationHub.Core/Components/MessageHandler.razor.cs
@@ -0,0 +1,84 @@
+﻿using Blazored.FluentValidation;
+using FluentValidation;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CommunicationHub.Core.Components
+{
+    public partial class MessageHandler : ComponentBase
+    {
+        [Inject] ISnackbar Snackbar { get; set; }
+
+
+        private FluentValidationValidator fluentValidator;
+        private bool messageSent;
+        public bool ValidateMessage;
+        private string newMessage;
+
+        public string Message { get; set; }
+
+        protected override void OnInitialized()
+        {
+            fluentValidator = new();
+            base.OnInitialized();
+        }
+
+        private async Task MessageChanged(string message)
+        {
+            ValidateMessage = false;
+            messageSent = false;
+            Message = message;
+        }
+
+        private async Task SendMessage()
+        {
+            ValidateMessage = true;
+            if (fluentValidator.Validate())
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    try
+                    {
+                        var uri = "http://RookieRockstar:8428/message/sendmessage"; // Url for local testing. Exchange for api url once live
+                        var requestData = new
+                        {
+                            Message = this.Message
+                        };
+
+                        string json = JsonConvert.SerializeObject(requestData);
+                        StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                        HttpResponseMessage response = await client.PostAsync(uri, content);
+                        response.EnsureSuccessStatusCode(); 
+
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        newMessage = responseBody;
+                        messageSent = true;
+                    }
+                    catch (HttpRequestException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        Snackbar.Add("Oops! Something went wrong", MudBlazor.Severity.Error);
+                    }
+                }
+
+            }
+        }
+    }
+
+    public class MessageHandlerValidator : AbstractValidator<MessageHandler>
+    {
+        public MessageHandlerValidator()
+        {
+            RuleFor(x => x.Message).NotEmpty().WithMessage("Message can not be empty").When(x => x.ValidateMessage);
+        }
+    }
+}
---
+++ CommunicationHub/Client/Program.cs
@@ -0,0 +1,26 @@
+using CommunicationHub.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomCenter;
+
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 5000;
+    config.SnackbarConfiguration.HideTransitionDuration = 200;
+    config.SnackbarConfiguration.ShowTransitionDuration = 200;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+});
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
